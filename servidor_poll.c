36ae57c05f500eb501599c73e0272f5bb7ec671c
KalengaAO <antoniopedrocalengar7@gmail.com>
2025-10-15 10:02:38 +0100

    servidor de troca de msg

--- servidor_poll.c
+++ servidor_poll.c
@@ -4,142 +4,139 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
 #include <unistd.h>
+#include <stdlib.h>
 
-#define MAX_LINE	4096
-#define LISTENQ		10
-#define MAX_CLIENT	1024
-#define	TIMEOUT		-1
-#define RED			"\033[1;31m"
-#define	GREEN		"\033[1;32m"
-#define YELLOW		"\033[1;33m"
-#define RESET		"\033[0m"
+#define	MAX_LINE	4096
+#define	LISTENQ		10
+#define	MAX_CLIENT	1024
 
-int	ft_server(char const *port)
+void	new_client(struct pollfd *canal, int lstfd, int *max)
 {
-	int						listenfd;
-	struct sockaddr_in		server;
+	int					connfd;
+	socklen_t			len;
+	int					it;
+	char				buff[MAX_LINE];
+	struct sockaddr_in	cliaddr;
 
-	listenfd = socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
-	memset(&server, 0, sizeof(server));
-	server.sin_family = AF_INET;
-	server.sin_addr.s_addr = htonl(INADDR_ANY);
-	server.sin_port = htons(atoi(port));
-	bind(listenfd, (struct sockaddr *)&server, sizeof(server));
-	listen(listenfd, LISTENQ);
-	return (listenfd);
-}
-
-void	ft_new_client(struct pollfd *client, int *max, int listenfd)
-{
-	struct	sockaddr_in		serv_client;
-	char					buff[MAX_LINE];
-	int						connfd;
-	socklen_t				len;
-	int						it;
-
-	connfd = accept(listenfd, (struct sockaddr *)&serv_client, &len);
-	printf(YELLOW "Nova conexão: ip: %s, porta %d\n" RESET,
-			inet_ntop(AF_INET, &serv_client.sin_addr, buff, sizeof(buff)),
-			ntohs(serv_client.sin_port));
-	it = 2;
-	while (it < MAX_LINE)
+	len = sizeof(cliaddr);
+	connfd = accept(lstfd, (struct sockaddr *)&cliaddr, &len);
+	printf("Nova conexão - ip: %s |  porta %d\n",
+			inet_ntop(AF_INET, (struct sockaddr *)&cliaddr.sin_addr, buff, sizeof(buff)),
+			ntohs(cliaddr.sin_port));
+	it = 0;
+	while (it < MAX_CLIENT)
 	{
-		if (client[it].fd < 0)
+		if (canal[it].fd < 0)
 		{
-			client[it].fd = connfd;
-			client[it].events = POLLIN;
+			canal[it].fd = connfd;
+			canal[it].events = POLLIN;
 			if (it > (*max))
 				(*max) = it;
 			return ;
 		}
 		++it;
 	}
-	fprintf(stderr, RED "%s\n" RESET, "limite maximo de cliente atingido");
-	close(connfd);
 }
 
-void	ft_handle_stdin(struct pollfd *client, int max)
+void	send_msg(struct pollfd *canal, int max)
 {
-	char	buff[MAX_LINE];
 	int		it;
+	int		connfd;
+	char	buff[MAX_LINE];
 
-	it = 0;
-	if (fgets(buff, sizeof(buff), stdin))
+	it = 2;
+	connfd = -1;
+	while (it <= max)
 	{
-		while (it <= max)
-		{
-			if (client[it].fd >= 0)
-				write(client[it].fd, buff, strlen(buff));
-			++it;
-		}
+		if (canal[it].revents & POLLIN)
+			connfd = canal[it].fd;
+		++it;
+	}
+	if (connfd != -1)
+	{
+		printf("use id %d: ", connfd);
+		if (fgets(buff, sizeof(buff), stdin))
+			write(connfd, buff, strlen(buff));
 	}
 }
 
-void	ft_handle_client(struct pollfd *client, int it)
+bool	read_msg(int connfd)
 {
+	int		rdbytes;
 	char	buff[MAX_LINE];
-	int		rbytes;
 
-	rbytes = read(client[it].fd, buff, sizeof(buff));
-	if (rbytes == 0)
+	if ((rdbytes = read(connfd, buff, MAX_LINE)) == 0)
 	{
-		printf(RED "Cliente %d desconectado\n" RESET, client[it].fd);
-		close(client[it].fd);
-		client[it].fd = -1;
-		return ;
+		printf("cliente desconectado %d\n", connfd);
+		close(connfd);
+		return false;
 	}
-	buff[rbytes] = '\0';
+	buff[rdbytes] = '\0';
+	printf("user: %d ", connfd);
 	write(STDOUT_FILENO, buff, strlen(buff));
+	return (true);
+}
+
+int	ft_server(char *port)
+{
+	int						lstfd;
+	struct sockaddr_in		server;
+
+	lstfd = socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
+	memset(&server, 0, sizeof(server));
+	server.sin_family = AF_INET;
+	server.sin_addr.s_addr = htonl(INADDR_ANY);
+	server.sin_port = htons(atoi(port));
+	bind(lstfd, (struct sockaddr *)&server, sizeof(server));
+	listen(lstfd, LISTENQ);
+	return (lstfd);
 }
 
 int	main(int argc, char *argv[])
 {
-	int						listenfd;
-	int						max;
-	int						it;
-	typedef struct	pollfd	t_pollfd;
-	t_pollfd	client[MAX_CLIENT];
+	int					lstfd;
+	int					max;
+	int					it;
+	struct	pollfd		canal[MAX_CLIENT];
+	
 
 	if (argc != 2)
 	{
-		fprintf(stderr, RED "Uso %s [porta]\n" RESET, argv[0]);
+		fprintf(stderr, "%s\n", "error: use: ./servidor_poll [porta]");
 		exit(EXIT_FAILURE);
 	}
-	listenfd = ft_server(argv[1]);
-	max = 0;
+	lstfd = ft_server(argv[1]);
 	it = 0;
 	while (it < MAX_CLIENT)
 	{
-		client[it].fd = -1;
+		canal[it].fd = -1;
 		++it;
 	}
-	client[0].fd = listenfd;
-	client[0].events = POLLIN;
-	client[1].fd = STDIN_FILENO;
-	client[1].events = POLLIN;
+	canal[0].fd = lstfd;
+	canal[0].events = POLLIN;
+	canal[1].fd = STDIN_FILENO;
+	canal[1].events = POLLIN;
 	max = 1;
 	while (true)
 	{
-		if (poll(client, max + 1, TIMEOUT) < 0)
-		{
-			perror("error in poll: ");
-			exit(EXIT_FAILURE);
-		}
-		if (client[0].revents & POLLIN)
-			ft_new_client(client, &max, listenfd);
-		if (client[1].revents & POLLIN)
-			ft_handle_stdin(client, max);
-		it = 0;
+		poll(canal, max + 1, -1);
+		if (canal[0].revents & POLLIN)
+			new_client(canal, lstfd, &max);
+		if (canal[1].revents & POLLIN)
+			send_msg(canal, max);
+		it = 2;
 		while (it <= max)
 		{
-			if (client[it].fd < 0)
-				continue ;
-			if (client[it].revents & POLLIN)
-				ft_handle_client(client, it);
+			if (canal[it].fd > 0)
+			{
+				if (canal[it].revents & POLLIN)
+					if (!read_msg(canal[it].fd))
+						canal[it].fd = -1;
+			}
 			++it;
 		}
-	}
-	return (0);	
+		canal[it].revents = 0;
+	}	
+	return (0);
 }
